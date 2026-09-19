233dfa3142142d0d90f42b14e73ecde68a3da7bf
Somnath <137623775+Somnathrosy@users.noreply.github.com>
2026-09-19 17:27:15 +0530

    feat: add Vite plugin to copy index.html to 404.html on build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-index-to-404',
+      closeBundle() {
+        fs.copyFileSync('dist/index.html', 'dist/404.html')
+      }
+    }
+  ],
   base: '/rs_flex_sinage/'
 })
